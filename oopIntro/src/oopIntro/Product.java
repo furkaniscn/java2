a1ae0ac7e1a4fd46f57200fe2cb2b5d3b03dea46
furkaniscn <82982366+furkaniscn@users.noreply.github.com>
2021-04-25 00:40:03 +0300

    Update Product.java

--- oopIntro/src/oopIntro/Product.java
+++ oopIntro/src/oopIntro/Product.java
@@ -2,14 +2,14 @@ package oopIntro;
 
 public class Product {
 	
-	public Product() { // Constructor=yapýcý kýsmýdýr ve bu kýsým yazmasanda her zaman vardýr
-		System.out.println("Ben çalýþtým"); // Fakat içine bir þey yazmak istiyorsan böyle yapmak zorundasýn
+	public Product() { 
+		System.out.println("Ben Ã§alÃ½Ã¾tÃ½m"); 
 	}
 	
 	public Product(int id,String name,double unitPrice,String detail) {
-		this(); // Baþka class çaðýrýlabilir üsttekini çaðýrdýk
-		this.id = id; // Bu classtaki id
-		this.name = name; // Bu clastaki name
+		this(); 
+		this.id = id; 
+		this.name = name; 
 		this.unitPrice = unitPrice;
 		this.detail = detail;
 	}
